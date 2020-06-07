810729f1824081d1ed1630e9946a4a2b80cb36a4
appdevgenie <appdevgenie@gmail.com>
2020-06-07 10:55:49 +0200

    update insert db functions

--- app/src/main/java/com/globomed/learn/AddEmployeeActivity.kt
+++ app/src/main/java/com/globomed/learn/AddEmployeeActivity.kt
@@ -2,7 +2,14 @@ package com.globomed.learn
 
 import android.app.Activity
 import android.app.DatePickerDialog
+import android.content.ContentValues
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import com.globomed.learn.GloboMedDBContract.EmployeeEntry.COLUMN_DESIGNATION
+import com.globomed.learn.GloboMedDBContract.EmployeeEntry.COLUMN_DOB
+import com.globomed.learn.GloboMedDBContract.EmployeeEntry.COLUMN_NAME
+import com.globomed.learn.GloboMedDBContract.EmployeeEntry.TABLE_NAME
 import kotlinx.android.synthetic.main.activity_add.*
 import java.text.SimpleDateFormat
 import java.util.*
@@ -10,10 +17,13 @@ import java.util.*
 class AddEmployeeActivity : Activity() {
 
     private val myCalendar = Calendar.getInstance()
+    private lateinit var databaseHelper: DatabaseHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add)
+
+        databaseHelper = DatabaseHelper(this)
 
         // on clicking ok on the calender dialog
         val date = DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
@@ -27,6 +37,48 @@ class AddEmployeeActivity : Activity() {
         etDOB.setOnClickListener {
             setUpCalender(date)
         }
+
+        bSave.setOnClickListener {
+            saveEmployee()
+        }
+
+        bCancel.setOnClickListener {
+            finish()
+        }
+    }
+
+    private fun saveEmployee() {
+        var isValid = true
+
+        etEmpName.error = if(etEmpName?.text.toString().isEmpty()){
+            isValid = false
+            "Required field"
+        } else null
+
+        etDesignation.error = if(etDesignation?.text.toString().isEmpty()){
+            isValid = false
+            "Required field"
+        } else null
+
+        if(isValid){
+            val name:String = etEmpName?.text.toString()
+            val designation:String = etDesignation?.text.toString()
+            val dob:Long = myCalendar.timeInMillis
+
+            val db = databaseHelper.writableDatabase
+            val values = ContentValues()
+            values.put(COLUMN_NAME, name)
+            values.put(COLUMN_DESIGNATION, designation)
+            values.put(COLUMN_DOB, dob)
+
+            val result = db.insert(TABLE_NAME, null, values)
+
+            setResult(RESULT_OK, Intent())
+
+            Toast.makeText(applicationContext, "Employee added", Toast.LENGTH_SHORT).show()
+        }
+
+        finish()
     }
 
     private fun setUpCalender(date: DatePickerDialog.OnDateSetListener) {
